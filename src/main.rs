50501847a264aed3a722759f72581c341bb6cb4c
Chris Fung <aergonaut@gmail.com>
2019-08-19 00:14:13 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "gear", about = "A developer QOL tool")]
+enum Gear {
+    #[structopt(name = "run", about = "Run user-defined commands")]
+    Run {
+        #[structopt(help = "Name of command to run")]
+        command: String,
+        #[structopt(help = "Optional arguments to pass to the command")]
+        args: Vec<String>,
+    },
+}
+
+fn main() {
+    let program = Gear::from_args();
+}
